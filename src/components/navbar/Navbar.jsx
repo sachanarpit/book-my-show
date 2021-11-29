2b3c08138be708e47029041191583c867fdd057b
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-29 14:29:43 +0530

    navbar ready

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <nav
+          className="navbar  navbar-expand-lg navbar-light top-navbar"
+          data-toggle="sticky-onscroll"
+        >
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              className="collapse navbar-collapse justify-content-end"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav pull-right">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Services
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Our Work
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="btn btn-primary" href="#">
+                    GET A QUOTE
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
