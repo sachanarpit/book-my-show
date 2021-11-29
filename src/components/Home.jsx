ceba900cc8f16de3146146dfc9580be40a0331e5
Arpit Sachan <sachanarpitknp@gmail.com>
2021-11-29 15:36:45 +0530

    Home Page Ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,28 @@
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 export const Home = () => {
-  return <div>home page</div>;
+  const AuthstateNow = useSelector((state) => state.AuthReducer);
+  console.log("stateNow:", AuthstateNow);
+  return (
+    <div>
+      {AuthstateNow === false ? (
+        <div>
+          {" "}
+          <h1>please login</h1>
+          <Link to="/login">
+            <button className="btn btn-danger">Login Now</button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <h1>Go to Dashboard to book ticket</h1>
+          <Link to="/dashboard">
+            <button className="btn btn-success">Dashboard</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
